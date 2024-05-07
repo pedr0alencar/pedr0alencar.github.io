2b606515f4116c955065294149c2cede7ca0a710
pedr0alencar <pedro_alencar@ufms.br>
2024-05-07 14:35:39 -0400

    Corrigindo erros de publicacao

---
+++ build/precache-manifest.e49bc443c4dec70e2755f82bc988998c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aaed6fcb51421b22ee3ca38785c942a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24ce3a66f9f6e063b1ef",
+    "url": "/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "8d300472a73d3e20bea3",
+    "url": "/static/css/main.d03cb13f.chunk.css"
+  },
+  {
+    "revision": "24ce3a66f9f6e063b1ef",
+    "url": "/static/js/2.eb9c02a4.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/static/js/2.eb9c02a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8d300472a73d3e20bea3",
+    "url": "/static/js/main.d97d5ab2.chunk.js"
+  },
+  {
+    "revision": "a2d41c56873c741b51b2",
+    "url": "/static/js/runtime-main.a5042f6b.js"
+  }
+]);
